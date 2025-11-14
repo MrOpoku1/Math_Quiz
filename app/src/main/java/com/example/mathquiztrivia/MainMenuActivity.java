1d5e52350f43a99447cf7d0e9917c8039dfaaec0
MrOpoku1 <asedaopoku3@gmail.com>
2025-11-14 17:46:25 -0500

    Fixed menu Button

--- app/src/main/java/com/example/mathquiztrivia/MainMenuActivity.java
+++ app/src/main/java/com/example/mathquiztrivia/MainMenuActivity.java
@@ -1,19 +1,13 @@
 package com.example.mathquiztrivia;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;   
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-    }
+        setContentView(R.layout.main_menu_activity);
+        }
 }
